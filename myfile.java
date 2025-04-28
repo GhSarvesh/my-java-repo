f0a90c086622ce533554201f1f6c2872bb49f2a3
Ubuntu <ubuntu@ip-172-31-1-245.ap-south-1.compute.internal>
2025-04-28 02:00:56 +0000

    javacommit2

--- myfile.java
+++ myfile.java
@@ -1,5 +1,5 @@
-public class HelloWorld {
+public class myfile {
     public static void main(String[] args) {
-        System.out.println("Hello, World and new update!");
+        System.out.println("Hello, World");
     }
 }
